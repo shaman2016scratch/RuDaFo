82695836153b3f681ec32e892acb95a6582e3867
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-03 22:11:50 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -12,9 +12,9 @@ RuDaFo = {
   "data": {},
   "FromJson": function(f, r) {
     if (f === 'object') {
-      RuDaFo.data.i = r
+      RuDaFo.data.i = r; RuDaFo.data.i4 = ""; RuDaFo.data.i3 = Object.keys(RuDaFo.data.i); for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i3.length; RuDaFo.data.i2++) { RuDaFo.data.i5 = RuDaFo.data.i[RuDaFo.data.i3[RuDaFo.data.i2]]; RuDaFo.data.i4 = `${RuDaFo.data.i4}${RuDaFo.data.i3[RuDaFo.data.i2]}:${RuDaFo.data.i5};` }; return RuDaFo.data.i4
     } else if (f === 'array') {
-      RuDaFo.data.i = r
+      RuDaFo.data.i = r; RuDaFo.data.i4 = ""; for(RuDaFo.data.i2 = 0; RuDaFo.data.i2 < RuDaFo.data.i.length; RuDaFo.data.i2++) { RuDaFo.data.i4 = `${RuDaFo.data.i4}${RuDaFo.data.i[RuDaFo.data.i2]}&` }; return RuDaFo.data.i4
     } else {
       console.error(`RuDaFo error: Not RuDaFo type name '${f}'`)
     }
